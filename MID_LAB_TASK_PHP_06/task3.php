44496195e0e3992460a5fde9acae96a1a2ac4be1
Fahad Hossain <hossainfahad76@gmail.com>
2021-02-23 12:48:36 +0600

    Task_6_3

---
+++ MID_LAB_TASK_PHP_06/task3.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+	<title>Change Password</title>
+</head>
+<body>
+	<form method="POST" action="">
+		<fieldset>
+			<legend>CHANGE PASSWORD</legend> <br>
+				<table>
+					<tr>
+						<td><img src="1.png" width="50px"></td>
+					</tr>
+					<tr>
+						<td>
+							<input type="file" name="image" >
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<input type="submit" name="submit" value="submit">
+						</td>
+					</tr>
+					
+				</table>
+		</fieldset>		
+	</form>
+</body>
+</html>
