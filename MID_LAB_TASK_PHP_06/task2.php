ca25b3c09cf975c9e6e8cac280c5949021a62c22
Fahad Hossain <hossainfahad76@gmail.com>
2021-02-24 20:52:17 +0600

    Task_6_2 Update

--- MID_LAB_TASK_PHP_06/task2.php
+++ MID_LAB_TASK_PHP_06/task2.php
@@ -1,11 +1,25 @@
 <?php
-	if(isset($_POST['submit'])){ 
+	if(isset($_POST['submit']) && isset($_POST['cpassword']) && isset($_POST['npassword']) && isset($_POST['rpassword'])){ 
 
-	echo "hello world";
-}
-	else{
-		echo "Invalid";
+		$cpass = $_POST['cpassword'];
+		$npass = $_POST['npassword'];
+		$rpass = $_POST['rpassword'];
+
+		if ($cpass != $npass ) {
+			if ($npass == $rpass) {
+				echo "Password Changed";
+			}
+			else{
+				echo "New Password must match with the Retyped Password";
+			}
+		}
+		else{
+			echo "New Password should not be same as the Current Password";
+		}
+
+	
 	}
+	
 
 ?>
 
@@ -21,20 +35,20 @@
 					<tr>
 						<td>Current Password:</td>	
 						<td>
-							<input type="password" name="password">
+							<input type="password" name="cpassword" value="">
 						</td>
 					</tr>
 					<tr>
 						<td>New Password:</td>		
 						<td>
-							<input type="password" name="password">
+							<input type="password" name="npassword" value=""> 
 							<br>
 						</td>
 					</tr>
 					<tr>
 						<td>Retype New Password:</td>		
 						<td>
-							<input type="password" name="password">
+							<input type="password" name="rpassword" value="">
 							<br>
 						</td>
 					<tr>
