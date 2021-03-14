1a37fac5603b7f478b682e585ae5e736a51881c9
Fahad Hossain <hossainfahad76@gmail.com>
2021-03-14 12:30:16 +0600

    1st commit at 12:30 PM

---
+++ MID_LAB_EXAM/registration.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<td>
+				<form>
+					<fieldset>
+						<legend>REGISTRATION</legend>
+						<table>
+							<tr>
+								<td>
+									Id
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="id" value="">
+								</td>
+							</tr>
+							<tr>
+								<td>
+									Password
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="password" name="password" value="">
+								</td>
+							</tr>
+							
+						</table>
+					</fieldset>
+				</form>
+			</td>
+		</tr>
+	</table>
+	
+</body>
+</html>
